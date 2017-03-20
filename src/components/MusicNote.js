c4f30088393dabbdcecd5a71341b2e12d8446a41
mnhuynh <mazie.huynh@gmail.com>
2017-03-20 12:19:46 -0400

    update music note parameters

---
+++ src/components/MusicNote.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as THREE from 'three';
+import Shape from './Shape';
+
+class MusicNote extends React.Component {
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        return (
+            <group>
+                <Shape
+                    resourceId="musicNote"
+                    color={new THREE.Color(Math.random() * 0xffffff)}
+                    x={0}
+                    y={0}
+                    z={0}
+                    rx={0}
+                    ry={0}
+                    rz={0}
+                    s={1}
+                />
+            </group>
+        )
+    }
+}
+
+export default MusicNote;
