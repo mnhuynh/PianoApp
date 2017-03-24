9bcd85b667ff781def6f78eca2c0458f5de04df5
mnhuynh <mazie.huynh@gmail.com>
2017-03-23 23:11:06 -0400

    update resources of shape

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -16,57 +16,49 @@ class Resources extends React.Component {
                 <shape resourceId="musicNote">
                     <moveTo
                         x={0}
-                        y={circleRadius}
-                    />
+                        y={circleRadius} />
                     {/*top L*/}
                     <quadraticCurveTo
                         cpX={circleRadius}
                         cpY={circleRadius}
                         x={circleRadius}
-                        y={0}
-                    />
+                        y={0} />
                     {/*bottom L*/}
                     <quadraticCurveTo
                         cpX={circleRadius}
                         cpY={-circleRadius}
                         x={0}
-                        y={-circleRadius}
-                    />
+                        y={-circleRadius} />
                     {/*bottom R*/}
                     <quadraticCurveTo
                         cpX={-circleRadius}
                         cpY={-circleRadius}
                         x={-circleRadius}
-                        y={0}
-                    />
+                        y={0} />
                     {/*vertical bar*/}
                     <quadraticCurveTo
                         cpX={x - 40}
                         cpY={y + 155}
                         x={x - 95}
-                        y={y + 45}
-                    />
+                        y={y + 45} />
                     {/*bar & curve*/}
                     <quadraticCurveTo
                         cpX={x - 95}
                         cpY={y + 115}
                         x={x - 35}
-                        y={y + 135}
-                    />
+                        y={y + 135} />
                     {/*top R connected to bar*/}
                     <quadraticCurveTo
                         cpX={x - 10}
                         cpY={y}
                         x={x - 15}
-                        y={y}
-                    />
+                        y={y} />
                     {/*top R*/}
                     <quadraticCurveTo
                         cpX={-circleRadius}
                         cpY={circleRadius}
                         x={0}
-                        y={circleRadius}
-                    />
+                        y={circleRadius} />
                 </shape>
             </resources>
         )
